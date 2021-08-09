4f904ec905f64e94925cf184071977029d19a9e7
Wisdom Ifeanyi <ifeanyiwisdom388@gmail.com>
2021-08-10 00:01:42 +0100

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,7 +1,7 @@
 package com.wisdom.Resturantmanagement.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.RestController;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
