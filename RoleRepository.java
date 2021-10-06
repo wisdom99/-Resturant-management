80936814d630c6f583e74f41589edc44cfb00bc9
Wisdom Ifeanyi <ifeanyiwisdom388@gmail.com>
2021-10-06 10:09:42 +0100

    Delete RoleRepository.java

--- RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wisdom.Resturantmanagement.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wisdom.Resturantmanagement.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
